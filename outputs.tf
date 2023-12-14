7205f020f4ffcca989b7ed10ec95290ddcbf105b
xneyder <xneyder@gmail.com>
2023-12-13 23:10:38 -0500

    Fix

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "acm_certificate_arn" {
+output "arn" {
   description = "The ARN of the certificate"
   value       = try(aws_acm_certificate_validation.this[0].certificate_arn, aws_acm_certificate.this[0].arn, "")
 }
